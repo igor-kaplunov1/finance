3bee502bd4d54a1466973d7dda5e72830f70c656
kapluig <kapluig@US013760499W10.local>
2017-01-13 15:47:43 -0500

    Added stock quote project

---
+++ stock-quote/src/main/java/org/sample/finance/stock_quote/App.java
@@ -0,0 +1,13 @@
+package org.sample.finance.stock_quote;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
